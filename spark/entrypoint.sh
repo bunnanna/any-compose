ad3b457f0109c9af187f5f141e042260f809d3cd
Nuttanont Na Ranong <y07025@autox.co.th>
2026-01-08 21:15:27 +0700

    fix spark cluter to connrct to jupyter lab

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 set -e
 
-# Default values
-SPARK_MASTER_HOST=${SPARK_MASTER_HOST:-spark-master}
-SPARK_MASTER_PORT=${SPARK_MASTER_PORT:-7077}
-SPARK_MASTER_URL="spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT}"
+export SPARK_HOME=/opt/spark
+export PYTHONPATH="$SPARK_HOME/python:$(ls $SPARK_HOME/python/lib/py4j-*.zip | head -n 1)"
 
-echo "*** Starting Spark Node in mode: $SPARK_MODE"
+ROLE=${SPARK_ROLE:-driver}
 
-if [ "$SPARK_MODE" = "master" ]; then
-    /opt/spark/bin/spark-class org.apache.spark.deploy.master.Master \
-        --host "$SPARK_MASTER_HOST" \
-        --port "$SPARK_MASTER_PORT" \
-        --webui-port "${SPARK_MASTER_WEBUI_PORT:-8080}"
+MASTER_HOST=${SPARK_MASTER_HOST:-spark-master}
+MASTER_PORT=${SPARK_MASTER_PORT:-7077}
+MASTER_URL="spark://${MASTER_HOST}:${MASTER_PORT}"
 
-elif [ "$SPARK_MODE" = "worker" ]; then
-    /opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
-        "$SPARK_MASTER_URL" \
-        --webui-port "${SPARK_WORKER_WEBUI_PORT:-0}"
+echo "================================="
+echo "Spark role : ${ROLE}"
+echo "Python     : $(which python)"
+echo "================================="
 
-else
-    echo "ERROR: SPARK_MODE must be master or worker"
+case "$ROLE" in
+  master)
+    exec /opt/spark/bin/spark-class \
+      org.apache.spark.deploy.master.Master \
+      --host "$MASTER_HOST" \
+      --port "$MASTER_PORT" \
+      --webui-port "${SPARK_MASTER_WEBUI_PORT:-8080}"
+    ;;
+
+  worker)
+    exec /opt/spark/bin/spark-class \
+      org.apache.spark.deploy.worker.Worker \
+      "$MASTER_URL" \
+      --webui-port "${SPARK_WORKER_WEBUI_PORT:-0}"
+    ;;
+
+  driver)
+    exec jupyter lab \
+  --ip=0.0.0.0 \
+  --port=8888 \
+  --no-browser \
+  --allow-root \
+  --ServerApp.token="${JUPYTER_TOKEN}"
+    ;;
+
+  *)
+    echo "Unknown SPARK_ROLE: $ROLE"
     exit 1
-fi
+    ;;
+esac
